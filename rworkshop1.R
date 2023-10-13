08d8baafb1c8299b6e06697effc58103d64fd88b
emilynngz <emilyningzizhu@outlook.com>
2023-10-13 12:49:47 +0100

    rworkshop

---
+++ rworkshop1.R
@@ -0,0 +1,6 @@
+v1<- c(1,2,3)
+v1
+v2 <- c("monday", "tuesday", "wednesday", "thursday")
+v2
+v3 <- c(1, "a", 2, "b", 3)
+v3
---
+++ rworkshop2.R
@@ -0,0 +1,3 @@
+for (time in c(1:5)) {
+  print(c("The course will", "finish", "in", time , "minutes" )) }
+
